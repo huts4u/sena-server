6f9b21247e277a27eb28e2d625f45b3f7defde78
huts4u <huts44u@gmail.com>
2025-03-22 12:48:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,18 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204); // Respond to preflight with success
+  }
+  
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
